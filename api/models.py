36bdbce4b130940dac65898538f6759dc5d11597
Meshdako <msobarzo@utem.cl>
2024-02-26 20:09:37 -0300

    UPDATE: Actualizado los modelos para que estos cumplan los requerimientos solicitados

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    # palabra ingresada y regla ocupada.
+    input_word = models.CharField(max_length = 50)
+    output_word = models.CharField(max_length = 50)
+    the_rule = models.PositiveBigIntegerField()
+    
+class Rules(models.Model):
+    rule_1 = models.PositiveBigIntegerField()
+    rule_2 = models.PositiveBigIntegerField()
+    rule_3 = models.PositiveBigIntegerField()
+    rule_4 = models.PositiveBigIntegerField()
