6e42cfdc6541e125eeeba50121516ab32c60694c
Meshdako <msobarzo@utem.cl>
2024-02-26 20:10:54 -0300

    UPDATE

--- Desafio_Tecnico.py
+++ Desafio_Tecnico.py
@@ -1,23 +1,35 @@
 def singular(palabra):
-  palabra = palabra.lower()
-  vocales = ["a", "e", "i", "o", "u"]
-  ultima = palabra[len(palabra) - 1]
-  # CASO 1
-  for vocal in vocales:
-    if ultima == vocal:
-      return (palabra + "s")
-  # CASO 2
-  if ultima == "x" or ultima == "s":
-    return (palabra)
-  # CASO 3
-  if ultima == "z":
-    palabra = palabra.replace("z", "ces")
-    return (palabra)
-  # CASO 4
-  else:
-    return (palabra + "es")
+    palabra = palabra.lower()
+    vocales = ["a", "e", "i", "o", "u"]
+    ultima = palabra[len(palabra) - 1]
+    # CASO 1
+    for vocal in vocales:
+        if ultima == vocal:
+            return (1, palabra + "s")
+    # CASO 2
+    if ultima == "x" or ultima == "s":
+        return (2, palabra)
+    # CASO 3
+    if ultima == "z":
+        palabra = palabra.replace("z", "ces")
+        return (3, palabra)
+    # CASO 4
+    else:
+        return (4, palabra + "es")
 
-palabra = "paréntesis"
-
-nuevaPalabra = singular(palabra)
-print (nuevaPalabra)
+def pluralizador(lasPalabras):
+    cantidadesPorRegla = [0, 0, 0, 0]
+    nuevasPalabras = []
+    
+    # Recorremos las Palbras ingresadas
+    for laPalabra in lasPalabras:
+        regla, nuevaPalabra = singular(laPalabra)
+        nuevasPalabras.append(nuevaPalabra)
+        cantidadesPorRegla[regla - 1] += 1
+      
+    print (nuevasPalabras)
+    print (cantidadesPorRegla)
+    
+palabras = ["CASA", "Perro", "limon"]
+print (palabras)
+pluralizador(palabras)
