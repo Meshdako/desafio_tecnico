edd440fd0e9ea3c0cc1cf7350d303331a9fff07f
Meshdako <msobarzo@utem.cl>
2024-02-27 03:03:08 -0300

    UPDATE: Borrado el parámetro the_rule del modelo Word, ya que no servía para nada

--- api/models.py
+++ api/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 class Word(models.Model):
     # palabra ingresada y regla ocupada.
-    input_word = models.CharField(max_length = 50)
-    output_word = models.CharField(max_length = 50)
-    the_rule = models.PositiveBigIntegerField()
+    input_word = models.CharField(max_length = 150)
+    output_word = models.CharField(max_length = 150)
     
 class Rules(models.Model):
     rule_1 = models.PositiveBigIntegerField()
